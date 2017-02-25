11e60e3a3aba5127f905e2652023052486d67a35
KyrieSu <watermelo0326@gmail.com>
2017-02-25 21:20:19 +0800

    finish HW1

--- HW1/main.py
+++ HW1/main.py
@@ -10,16 +10,35 @@ img = [img1,img2]
 for i in xrange(len(img)):
     cv2.namedWindow(title[i],cv2.WINDOW_NORMAL)
     cv2.imshow(title[i],img[i])
+
 row,col,pix = img1.shape
 H , W = img2.shape
 
-img1[:,:,:] = img1[:,:,:] - val;
-img2[:,:] = img2[:,:] - val;
+# img1[:,:,:] = img1[:,:,:] - val;
+# decrease value for each im img1
+for i in xrange(row):
+    for j in xrange(col):
+        for k in xrange(pix):
+            if img1[i][j][k] > val:
+                img1[i][j][k] = img1[i][j][k] - val
+            else:
+                pass
+
+#img2[:,:] = img2[:,:] - val
+#decrease value for each in img2
+for i in xrange(H):
+    for j in xrange(W):
+        if img2[i][j] > val:
+            img2[i][j]= img2[i][j] - val
+        else:
+            pass
 
 title = ['AFTER_image1','AFTER_image2']
 
 for i in xrange(len(img)):
     cv2.namedWindow(title[i],cv2.WINDOW_NORMAL)
     cv2.imshow(title[i],img[i])
+    cv2.imwrite(title[i]+'.jpg',img[i])
+
 cv2.waitKey(0)
 cv2.destroyAllWindows()
