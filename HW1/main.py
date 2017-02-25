9fc6a7c41f9ec0f293fb6d49c1fc87d2b7188347
KyrieSu <watermelo0326@gmail.com>
2017-02-25 21:01:58 +0800

    small bug in stbtract value

--- HW1/main.py
+++ HW1/main.py
@@ -1,2 +1,25 @@
 import cv2
 import numpy as np
+
+img1 = cv2.imread('1.png',1) # BGR mode
+img2 = cv2.imread('2.jpg',0) # gray mode
+val = int(raw_input('Enter a value from each pixel : ')) #convert string to int
+
+title = ['image1','image2']
+img = [img1,img2]
+for i in xrange(len(img)):
+    cv2.namedWindow(title[i],cv2.WINDOW_NORMAL)
+    cv2.imshow(title[i],img[i])
+row,col,pix = img1.shape
+H , W = img2.shape
+
+img1[:,:,:] = img1[:,:,:] - val;
+img2[:,:] = img2[:,:] - val;
+
+title = ['AFTER_image1','AFTER_image2']
+
+for i in xrange(len(img)):
+    cv2.namedWindow(title[i],cv2.WINDOW_NORMAL)
+    cv2.imshow(title[i],img[i])
+cv2.waitKey(0)
+cv2.destroyAllWindows()
