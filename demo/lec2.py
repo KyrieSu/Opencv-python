507df50c23cdcd11d49902fb075fac2d61e9f249
KyrieSu <watermelo0326@gmail.com>
2017-03-06 13:31:00 +0800

    clear demo and apply ML

--- demo/lec2.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-import cv2
-from matplotlib import pyplot as plt
-
-img = cv2.imread("Taipei.jpg",0)
-plt.imshow(img,cmap='gray',interpolation='bicubic')
-plt.xticks([1200]),plt.yticks([100])
-plt.show()
