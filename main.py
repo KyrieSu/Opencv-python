790011de7bd701ec5ea655d60fbdcc6445b70bdb
KyrieSu <watermelo0326@gmail.com>
2017-03-12 13:22:04 +0800

    fix bug in search gray row

--- main.py
+++ main.py
@@ -1,48 +1,39 @@
 import cv2
 import os
-import random
 import numpy as np
 
 def check(img):
-    # avoid exception
     if img is None:
         return False
-    row , col , pix = img.shape
-    # random 10 element to sample
-    gray = np.array([128,128,128])
-    H = 0
-    while H<row:
-        if np.array_equal(gray,img[H,0]):
+    row, col, pix = img.shape
+    gray = np.array([128, 128, 128])
+    H = row-1
+    while H>=0:
+        if np.array_equal(gray, img[H, 0]):
             break;
-        H+=1;
-    if H==row:
+        H -= 1;
+    if H==-1: # not find gray
         return False
-    count  = 0
-    for i in xrange(col-1):
-        if np.array_equal(img[H,i],img[H,i+1]):
-            count +=1
-    if count > (col/2):
-        return True
-    else:
-        return False
-
-    # for i in xrange(0,col-1,1):
-    #     if not (np.array_equal(gray,img[H,i]) and np.array_equal(img[H,i],img[H,i+1])):
-    #         return False
-    # return True
-
+    count = 0
+    for i in xrange(col):
+        if np.array_equal(img[H,i],gray):
+            count += 1
+            if count > (col/2):
+                return True
+    return False
 
 if __name__ == "__main__":
-    count = 0
-    path = '/Volumes/Transcend/Opencv-python/272'
+    path = '/Volumes/Transcend/Opencv-python/user'
     for roots,dirs,files in os.walk(path):
-        for f in files:
-            img = cv2.imread(r'272/'+f,1)
-            if check(img):
-                print f
-                # img = cv2.imshow('ERR',img)
-                # cv2.waitKey(0)
-                count+=1
-                print count
-            else:
-                pass
+        for dir in dirs:
+            tmp_path = path + '/' + dir
+            count = 0
+            for roots,dirs,files in os.walk(tmp_path):
+                for f in files:
+                    img = cv2.imread(tmp_path + '/' + f)
+                    if check(img):
+                        count+=1
+                        print f
+                    else:
+                        print "good:" , f
+                print 'I am in ' + tmp_path + ' : ' , count
