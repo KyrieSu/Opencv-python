d68eaf3c89a0a744430e90929fed02d528ec1420
KyrieSu <watermelo0326@gmail.com>
2017-04-15 14:05:17 +0800

    implement Sobel

--- HW4/sobel.py
+++ HW4/sobel.py
@@ -19,23 +19,26 @@ res = np.zeros(image.shape)
 #              (sobelx[1][1] * image[i][j]) + (sobelx[1][2] * image[i][j+1]) + \
 #              (sobelx[2][0] * image[i+1][j-1]) + (sobelx[2][1] * image[i+1][j]) + \
 #              (sobelx[2][2] * image[i+1][j+1])
-#
+
 #         gy = (sobely[0][0] * image[i-1][j-1]) + (sobely[0][1] * image[i-1][j]) + \
 #              (sobely[0][2] * image[i-1][j+1]) + (sobely[1][0] * image[i][j-1]) + \
 #              (sobely[1][1] * image[i][j]) + (sobely[1][2] * image[i][j+1]) + \
 #              (sobely[2][0] * image[i+1][j-1]) + (sobely[2][1] * image[i+1][j]) + \
 #              (sobely[2][2] * image[i+1][j+1])
-#
+
 #         x_img[i-1][j-1] = gx
 #         y_img[i-1][j-1] = gy
-#
+
 #         #Calculate the gradient magnitude
+#         # g = np.absolute(gx) + np.absolute(gy)
 #         g = np.sqrt(gx * gx + gy * gy)
 #         res[i-1][j-1] = g
 
 x_img = cv2.filter2D(image,-1,sobelx)
 y_img = cv2.filter2D(image,-1,sobely)
 res = cv2.addWeighted(x_img,0.5,y_img,0.5,0)
+# res = np.sqrt(np.square(x_img)+np.square(y_img))
+# res = res.astype(int)
 edges = cv2.Canny(image,100,200)
 cv2.imshow('Gx',x_img)
 cv2.imshow('Gy',y_img)
