5261bad59f46d093a540898c577a160339c69e74
KyrieSu <watermelo0326@gmail.com>
2017-06-12 19:50:01 +0800

    finish HW7

--- HW7/main.py
+++ HW7/main.py
@@ -1,19 +1,21 @@
 import cv2
 import numpy as np
+import sys
 
-def show(img):
-    dst = cv2.resize(img,(800,600))
-    cv2.imshow('show',dst)
+
+if __name__ == '__main__':
+    filename = sys.argv[1]
+    img = cv2.imread(filename,0)
+    img = cv2.resize(img,(800,600))
+    img = cv2.medianBlur(img,5)
+    ret , cimg = cv2.threshold(img,0,255,cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)
+    ret , img = cv2.threshold(img,ret-70,255,cv2.THRESH_BINARY_INV)
+    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(5,5))
+    img = cv2.dilate(img,kernel,iterations = 1)
+    img = cv2.erode(img,kernel,iterations = 10)
+    num , label , stats , centroid = cv2.connectedComponentsWithStats(img,4,cv2.CV_32S)
+    print num
+    cv2.imshow('test',img)
     cv2.waitKey(0)
-    cv2.destroyWindow('show')
-
-src = cv2.imread('1.jpg')
-img = cv2.cvtColor(src,cv2.COLOR_BGR2GRAY)
-ret , img = cv2.threshold(img,127,255,cv2.THRESH_BINARY_INV)
-
-k = np.ones((5,5),np.uint8)
-img = cv2.erode(img,k,iterations = 10)
-img = cv2.morphologyEx(img, cv2.MORPH_OPEN, k)
-
-
-show(img)
+    cv2.destroyAllWindows()
+    
